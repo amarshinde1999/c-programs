977c979b5561681bf67386fa6e60dd40846991d7
amarshinde1999 <106017882+amarshinde1999@users.noreply.github.com>
2022-07-30 12:34:52 +0530

    Add files via upload

---
+++ asmt3(2).c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void DisplayEvenfactor(int iNo)
+{
+	int i=0;
+	if(iNo<=0)
+	{
+		iNo=-iNo;
+	}
+	for(i=1; i<=iNo/2; i++)
+	{
+		if(i%2==0)
+		{
+			printf("%d",i);
+		}			
+	}
+}
+int main()
+{
+	int iValue=0;
+	printf("enter number\n");
+	scanf("%d",&iValue);
+	
+	DisplayEvenfactor(iValue);
+	
+	return 0;
+}
